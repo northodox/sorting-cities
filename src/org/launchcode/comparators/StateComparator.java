95e4c9edd739c32f0895cea7de2a6a300a606be2
Chris <northodontist@gmail.com>
2019-06-17 18:44:59 -0500

    I...think I got the state comparator to work, committing before testing.

--- src/org/launchcode/comparators/StateComparator.java
+++ src/org/launchcode/comparators/StateComparator.java
@@ -1,4 +1,13 @@
 package org.launchcode.comparators;
 
-public class StateComparator {
+import org.launchcode.City;
+
+import java.util.Comparator;
+
+public class StateComparator implements Comparator<City> {
+
+    @Override
+    public int compare(City a, City b) {
+        return a.getState().compareTo(b.getState());
+    }
 }
